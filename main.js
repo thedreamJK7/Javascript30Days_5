20e20a6126213397f8f38602c56ac051b7b689b2
thedreamJK7 <jkubaev7@gmail.com>
2022-02-10 03:13:39 +0500

    end

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-const btn = document.querySelectorAll('btn');
+const btn = document.querySelectorAll('.btn');
+const main = document.querySelector('main');
+const contentBox = document.querySelectorAll('.content-box');
+
+main.addEventListener('click', (e)=> {
+    const id = e.target.dataset.id;
+    if(id) {
+        btn.forEach(element => {
+            element.classList.remove('active');
+        });
+        e.target.classList.add('active');
+        
+        contentBox.forEach(element => {
+            element.classList.remove('active'); 
+        });
+    }
+
+    const activeList = document.getElementById(id);
+    activeList.classList.add('active');
+})
